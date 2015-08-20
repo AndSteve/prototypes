09a470f2ccae6d1233e9216cb2ddbc689d393c68
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-20 10:59:10 -0700

    made carousel

--- php_directory_operations/carousel.php
+++ php_directory_operations/carousel.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <title>Carousel</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script>
         var image_array = [];
+        var current_image = 0;
         $(document).ready(function () {
             load_files();
+            $('#previous').click(function () {
+                prev_image();
+            })
+            $('#next').click(function () {
+                next_image();
+            })
         });
         function load_files() {
             console.log('load files was executed');
@@ -15,42 +23,79 @@
                 success: function (response) {
                     if (response.success === true) {
                         createCarousel(response.files);
-                    } else {
-                        alert('There was an oopsie.');
                     }
                 }
             })
-        };
+        }
+        ;
         function createCarousel(images) {
             for (var i = 0; i < images.length; i++) {
-                var picture = $('<img>', {
-                    src: images[i],
-                    class: 'carImage'
-                });
+                var picture = $('<img>').attr("src", images[i]);
                 $('#image_container').append(picture);
-                image_array.push(images[i]);
-            }console.log(image_array)
-        };
+                image_array.push(picture);
+            }
+        }
+        function prev_image() {
+            var previous;
+
+            if (current_image === 0) {
+                previous = image_array.length - 1;
+            }
+            else {
+                previous = current_image - 1;
+            }
+            image_array[current_image].animate({
+                'left': '100%'
+            }, 500);
+            image_array[previous].css("left", "-100%");
+            image_array[previous].animate({
+                'left': '0%'
+            }, 500);
+            console.log("current_image_index is : " + current_image + " previous is : " + previous);
+            current_image = previous;
+        }
+        function next_image() {
+            var next;
+
+            if (current_image === image_array.length - 1) {
+                next = 0;
+            }
+            else {
+                next = current_image + 1;
+            }
+            image_array[current_image].animate({
+                'left': '-100%'
+            }, 500);
+
+            image_array[next].css("left", "100%");
+            image_array[next].animate({
+                'left': '0%'
+            }, 500);
+            current_image = next;
+        }
+
     </script>
     <style>
         #image_container {
-            position:relative;
-            height: 300px;
-            width: 400px;
+            position: relative;
+            height: 600px;
+            width: 800px;
+            display: block;
         }
-        .carImage {
-            position:absolute;
-            height:100%;
-            width:100%;
+
+        img {
+            position: absolute;
+            height: 100%;
+            width: 100%;
         }
     </style>
 
 </head>
 <body>
-<button type="button" id="previous"><-</button>
-<button type="button" id="right">-></button>
-<div id="image_container">
 
+<div id="image_container">
 </div>
+<button type="button" id="previous"><-</button>
+<button type="button" id="next">-></button>
 </body>
 </html>
