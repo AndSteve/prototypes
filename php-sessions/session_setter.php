44f9a9d57e38329cf02cd029b81c048c8a0a0d5a
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-21 11:09:07 -0700

    started prototype

---
+++ php-sessions/session_setter.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<form action="session_reader.php" method="get">
+    <input type="text" name="name" placeholder="Name">
+    <input type="number" name="age" placeholder="Age">
+    <input type="text" name="occupation" placeholder="Occupation">
+    <button type="submit">Submit</button>
+</form>
+</form>
+</body>
+</html>
