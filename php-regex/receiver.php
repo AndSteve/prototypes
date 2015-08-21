9dded860a5ca7f53197f540ddcd1239c64dbf6ac
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-21 12:34:47 -0700

    proto 3 mostly done

--- php-regex/receiver.php
+++ php-regex/receiver.php
@@ -5,28 +5,49 @@ foreach ($_POST as $key => $value) {
         case 'first_name':
             print('Checking first name: ' . $value);
             if (preg_match('/^[a-zA-Z]{2,}$/', $value)) {
-                print(' This is a valid name');
+                print('<br>This is a valid name');
             } else {
-                print('Invalid Name');
+                print('<br>Invalid Name');
+                global $errors;
+                $errors[] = "invalidName " . $value;
             }
             break;
         case 'age':
             print('Checking age: ' . $value);
-            if (preg_match('/[0-9]{0,3}$/', $value)){
-                print(' This is a valid age');
+            if (preg_match('/^(\d?[1-9]|[1-9]0)$/', $value)) {
+                print('<br>This is a valid age');
             } else {
-                print('This is not a valid age');
+                print('<br>This is not a valid age');
+                global $errors;
+                $errors[] = "invalidAge " . $value;
             }
             break;
         case 'phone':
             print('Checking phone number: ' . $value);
+            if (preg_match('/(1[0-9]{9})|((\([0-9]){3}\) ([0-9]){3}-([0-9]){4})|([0-9]){3}-([0-9]){3}-([0-9]){4}$/', $value)) {
+                print('<br>This is a valid phone number');
+            } else {
+                print('Maybe not a real number');
+            }
             break;
         case 'username':
             print('Checking username: ' . $value);
+            if (preg_match('/(^[a-zA-Z])([a-zA-Z0-9]{5})/', $value)) {
+                print('Valid username');
+            } else {
+                print('invalid username');
+            }
             break;
         case 'bonus':
             print('Checking bonus: ' . $value);
+            if (preg_match('/[0-9]{3}/', $value)) {
+                print('Valid bonus');
+            } else {
+                print('invalid bonus');
+            }
             break;
+        default :
+            print('<br><strong>Something went wrong?</strong><br>');
     }
     print('<br>');
 }
