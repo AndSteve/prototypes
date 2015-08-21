6c708c1214fcea7076d2f2f2456ac521be686534
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-21 11:03:38 -0700

    working on prototype 3

--- php-regex/receiver.php
+++ php-regex/receiver.php
@@ -1,4 +1,32 @@
 <?php
-foreach($_POST as $key => $value){
-    print('<br>Key: '.$key.': '.$value);
+foreach ($_POST as $key => $value) {
+//    print('<br>Key: '.$key.': '.$value);
+    switch ($key) {
+        case 'first_name':
+            print('Checking first name: ' . $value);
+            if (preg_match('/^[a-zA-Z]{2,}$/', $value)) {
+                print(' This is a valid name');
+            } else {
+                print('Invalid Name');
+            }
+            break;
+        case 'age':
+            print('Checking age: ' . $value);
+            if (preg_match('/[0-9]{0,3}$/', $value)){
+                print(' This is a valid age');
+            } else {
+                print('This is not a valid age');
+            }
+            break;
+        case 'phone':
+            print('Checking phone number: ' . $value);
+            break;
+        case 'username':
+            print('Checking username: ' . $value);
+            break;
+        case 'bonus':
+            print('Checking bonus: ' . $value);
+            break;
+    }
+    print('<br>');
 }
