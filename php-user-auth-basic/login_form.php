9ab35330ec1031c55aa5ed6d3abec6dd460a272d
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-23 21:28:18 -0700

    prototype 1

--- php-user-auth-basic/login_form.php
+++ php-user-auth-basic/login_form.php
@@ -1,1 +1,35 @@
 <!--login_form.php-->
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <title>Basic Login Form</title>
+    <link type="image/png" rel="icon" href="https://cdn3.iconfinder.com/data/icons/watchify-v1-0-32px/32/lock-32.png">
+    <script>
+        $(document).ready(function(){
+            $("#submit").click(function(){
+                var userName = $('#userName').val();
+                var pass = $('#pass').val();
+                $.ajax({
+                    method: "POST",
+                    url: "login_handler.php",
+                    cache: false,
+                    data: {username: userName, password: pass},
+                    dataType: "text",
+                    success: function(response){
+                        console.log(response);
+                        alert('Successfully Logged In');
+                    }
+                })
+            })
+        })
+    </script>
+</head>
+<body>
+<form action="login_handler.php" method="POST">
+    <input id="userName" type="text" name="userName" placeholder="Username">
+    <input id="pass" type="password" name="password" placeholder="Password">
+    <button id="submit" type="submit">Submit</button>
+</form>
+</body>
+</html>
--- php-user-auth-basic/login_handler.php
+++ php-user-auth-basic/login_handler.php
@@ -1,1 +1,23 @@
 <!--login_handler.php-->
+<?php
+$user_info = [
+    ['id' => 0, 'userName' => 'BurgerBob', 'password' => 'passwordlol'],
+    ['id' => 0, 'userName' => 'waterboy', 'password' => 'abc123'],
+    ['id' => 0, 'userName' => 'pinky', 'password' => 'drugsaregood'],
+    ['id' => 0, 'userName' => 'brain', 'password' => 'worldomination'],
+    ['id' => 0, 'userName' => 'annie', 'password' => 'bunnies'],
+];
+
+$userName = $_POST['username'];
+$passWord = $_POST['password'];
+
+for ($i = 0; $i < count($user_info); $i++) {
+    if ($userName == $user_info[$i]['userName']) {
+        if ($passWord == $user_info[$i]['password']) {
+            $_SESSION['user_id'] = rand(1, 10000000000);
+            print('Successfully logged in!');
+        } else {
+            print('Username or password was incorrect, try again');
+        }
+    }
+}
