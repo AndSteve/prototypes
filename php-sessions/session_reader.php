ab2dfad4d7078234168cfc1435933ecdad47bea0
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-27 16:56:48 -0700

    fixed up

--- php-sessions/session_reader.php
+++ php-sessions/session_reader.php
@@ -5,16 +5,16 @@ $age = $_POST['age'];
 $occupation = $_POST['occupation'];
 $_SESSION['error'] = '';
 if (preg_match("/^[a-zA-Z]{3,}$/", $name)) {
-    $_SESSIONS['name'] = $_POST['name'];
+    $_SESSION['name'] = $_POST['name'];
 } else {
     $_SESSION['error']['name'] = $_POST['name'];
 }
 if (preg_match("/^[0-9]{1,2}$/", $age)) {
-    $_SESSION['age'] = $_POST['name'];
+    $_SESSION['age'] = $_POST['age'];
 } else {
-    $_SESSION['error']['name'] = $_POST['$ge'];
+    $_SESSION['error']['age'] = $_POST['age'];
 }
-if (preg_match("/^[a-zA-Z]{3,99}&/", $occupation)) {
+if (preg_match("/^[a-zA-Z]{3,99}$/", $occupation)) {
     $_SESSION['occupation'] = $_POST['occupation'];
 } else {
     $_SESSION['error']['occupation'] = $_POST['occupation'];
--- php-sessions/session_setter.php
+++ php-sessions/session_setter.php
@@ -1,3 +1,4 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,10 +7,21 @@
 <body>
 <form action="session_reader.php" method="POST">
     <input type="text" name="name" placeholder="Name">
+    <?php if (isset($_SESSION['error']['name'])) {
+        print($_SESSION['error']['name']);
+        print('<br>');
+    } ?>
     <input type="number" name="age" placeholder="Age">
+    <?php if (isset($_SESSION['error']['age'])) {
+        print($_SESSION['error']['age']);
+        print('<br>');
+    } ?>
     <input type="text" name="occupation" placeholder="Occupation">
+    <?php if (isset($_SESSION['error']['occupation'])) {
+        print($_SESSION['error']['occupation']);
+        print('<br>');
+    } ?>
     <button type="submit">Submit</button>
-</form>
 </form>
 </body>
 </html>
