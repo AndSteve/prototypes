0260107bf9cd8da7ff003e453e834901c6baf0e1
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-28 15:27:09 -0700

    proto2

--- php-mysql/index_select.php
+++ php-mysql/index_select.php
@@ -1,3 +1,10 @@
 <?php
 require('mysql_connect.php');
-$query = [];
+$query = 'SELECT * FROM todo_items2 WHERE user_id = 12';
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        $output[] = $row;
+    }
+}
+print_r($output);
