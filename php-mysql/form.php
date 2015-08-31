ac30992d50d39286f0916e0818a43c86debf1c91
Andrew Lopez <andrewlopez88@gmail.com>
2015-08-31 10:57:31 -0700

    posting correctly

---
+++ php-mysql/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php-mysql prototype</title>
+</head>
+<body>
+<form action="index_insert.php" method="post">
+    <input type="text" name="title" placeholder="Title">
+    <textarea name="details" placeholder="Details"></textarea>
+    <input type="time" name="time">
+    <select name="dingdong">
+        <option>Ding</option>
+        <option>Dong</option>
+    </select>
+    <button>Submit</button>
+</form>
+</body>
+</html>
--- php-mysql/index_insert.php
+++ php-mysql/index_insert.php
@@ -1,1 +1,8 @@
-<!-- index_insert.php -->
+<?php
+print_r($_POST);
+require('mysql_connect.php');
+$query = "INSERT INTO todo_items2 SET title = 'Play Badminton', details='hit some cocks around', timestamp = 12, user_id = 12";
+//mysqli_query($conn, $query);
+if (mysqli_affected_rows($conn) > 0) {
+    print(mysqli_affected_rows($conn));
+}
